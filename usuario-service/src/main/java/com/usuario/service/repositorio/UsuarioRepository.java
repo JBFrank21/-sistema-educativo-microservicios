6a14c0a13ac031a7c9c37bb77278fcb91a60c541
JBFrank21 <140364806+JBFrank21@users.noreply.github.com>
2025-04-14 23:53:01 -0500

    eureka

--- usuario-service/src/main/java/com/usuario/service/repositorio/UsuarioRepository.java
+++ usuario-service/src/main/java/com/usuario/service/repositorio/UsuarioRepository.java
@@ -1,11 +1,11 @@
 package com.usuario.service.repositorio;
- 
- import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.stereotype.Repository;
- 
- import com.usuario.service.entidades.Usuario;
- 
- @Repository
- public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
- 
- }
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.usuario.service.entidades.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+}
